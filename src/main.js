3d9108238e278ceb437e2e7955f423d2e567033d
meiffe <meifee@163.com>
2020-11-05 21:37:46 +0800

    数据处理

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import router from "./router";
+import store from "./store";
+import css from "./css/reset.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
+Vue.use(css);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    el: "#app",
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount("#app");
